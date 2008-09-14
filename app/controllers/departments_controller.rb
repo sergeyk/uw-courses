9f10c4af6754830c22c5b0b2fea75f94be8f231f
iamthedivebomber <iamthedivebomber@gmail.com>
2008-09-14 11:39:07 -0700

    fixed a departments controller bug and experimenting with httperf also

--- app/controllers/departments_controller.rb
+++ app/controllers/departments_controller.rb
@@ -4,14 +4,7 @@ class DepartmentsController < ApplicationController
   end
   
   def show
-    @department = Department.find(params[:id])
-    
-    @num_evaluations = @department.number_of_evaluations
-    @average_overall_rating = @department.average_overall_rating
-    @average_instructor_specific_rating = @department.average_instructor_specific_rating
-    @average_course_specific_rating = @department.average_course_specific_rating
-    @average_grading_rating = @department.average_grading_rating
-    
-    @evaluations = @department.paginated_evaluations(params[:page])
+    @hme = Department.find(params[:id])    
+    @evaluations = @hme.paginated_evaluations(params[:page])
   end
 end
